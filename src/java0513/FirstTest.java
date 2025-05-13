e0697b8c11f931bcd352706baf12c39a9a997c95
najasinis <145651124+najasinis@users.noreply.github.com>
2025-05-13 09:01:42 +0900

    create file

---
+++ src/java0513/FirstTest.java
@@ -0,0 +1,12 @@
+package java0513;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		System.out.println("Java");
+		System.out.print("Java");
+		// TODO Auto-generated method stub
+
+	}
+
+}
